0028ca6eb200c710df19d5b2ed00859816884a5d
DuckInTub <icebl0804@gmail.com>
2023-12-07 12:09:15 +0100

    Added python solution for day 7

---
+++ 7/part2.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+def get_hand_type(hand):
+    count = Counter(hand.replace("J", ""))
+    if hand == "JJJJJ":
+        return 7
+    if len(count.values()) > 1:
+        max_count, next_count = sorted(count.values(), reverse=True)[:2]
+    else:
+        max_count, next_count = max(count.values()), -1
+
+    if "J" in hand:
+        max_count += Counter(hand)["J"]
+
+    # Five of a kind
+    if max_count == 5:
+        return 7
+    # Four of a kind
+    if max_count == 4:
+        return 6
+    # Full house
+    if max_count == 3 and next_count == 2:
+        return 5
+    # Three of a kind
+    if max_count == 3:
+        return 4
+    # Two pairs
+    if max_count == 2 and next_count == 2:
+        return 3
+    # One pair
+    if max_count == 2:
+        return 2
+    # Junk
+    return 1
+
+def hand_key(handbid):
+    values = ["A", "K", "Q", "T", "9", "8", "7", "6", "5", "4", "3", "2", "J"]
+    values = list(reversed(values))
+    hand, bid = handbid
+    t = get_hand_type(hand)
+    order = [values.index(card) for card in hand]
+    return (t, order)
+
+with open("input.txt", "r") as file:
+    data = [line.split() for line in file.read().splitlines()]
+
+assert get_hand_type("TTTTJ") == 7
+assert get_hand_type("TTKKJ") == 5
+
+data = list(sorted(data, key=hand_key))
+score = 0
+for i, handbid in enumerate(data):
+    score += (i+1)*int(handbid[1])
+
+print(score)
---
+++ 7/sol.py
@@ -0,0 +1,48 @@
+from collections import Counter
+
+def get_hand_type(hand):
+    count = Counter(hand)
+    if len(count.values()) > 1:
+        max_count, next_count = sorted(count.values(), reverse=True)[:2]
+    else:
+        max_count, next_count = max(count.values()), -1
+
+    # Five of a kind
+    if max_count == 5:
+        return 7
+    # Four of a kind
+    if max_count == 4:
+        return 6
+    # Full house
+    if max_count == 3 and next_count == 2:
+        return 5
+    # Three of a kind
+    if max_count == 3:
+        return 4
+    # Two pairs
+    if max_count == 2 and next_count == 2:
+        return 3
+    # One pair
+    if max_count == 2:
+        return 2
+    # Junk
+    return 1
+
+def hand_key(handbid):
+    values = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"]
+    values = list(reversed(values))
+    hand, bid = handbid
+    t = get_hand_type(hand)
+    order = [values.index(card) for card in hand]
+    return (t, order)
+
+with open("input.txt", "r") as file:
+    data = [line.split() for line in file.read().splitlines()]
+
+data = list(sorted(data, key=hand_key))
+
+score = 0
+for i, handbid in enumerate(data):
+    score += (i+1)*int(handbid[1])
+
+print(score)
