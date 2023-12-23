20d2d529149bcc6bada24cef2aab1d752014bd45
DuckInTub <icebl0804@gmail.com>
2023-12-23 14:00:50 +0100

    Day 23 full

--- 23/sol.py
+++ 23/sol.py
@@ -1,5 +1,7 @@
 from collections import deque
 from copy import deepcopy
+from heapq import heappop, heappush
+from typing import Dict, Tuple
 
 UP = (-1, 0)
 DOWN = (1, 0)
@@ -60,6 +62,43 @@ def longest_path_slip(steps, start, stop, seen):
                 mx = max(mx, longest_path_slip(steps+1, new, stop, set(seen)))
         return mx
 
+def walk_to_intersection(start, intersections):
+    seen = set()
+    Q = deque([(0, start)])
+    dists = {}
+    while Q:
+        steps, at = Q.pop()
+        if at in seen:
+            continue
+        seen.add(at)
+        if at in intersections - {start}:
+            dists[at] = steps
+            continue
+
+        for d in DIRS:
+            new = add_points(d, at)
+            nr, nc = new
+            if not (0 <= nr < len(data) and 0 < nc <= len(data[0])):
+                continue
+            if new not in seen and data[nr][nc] != '#':
+                Q.append((steps+1, new))
+    
+    return dists
+
+def longest_path(start, stop, graph : Dict[Tuple[int, int], Dict[Tuple[int, int], int]]):
+    def dfs(curr, seen):
+        if curr == stop:
+            return 0
+        seen.add(curr)
+        mx = 0
+        for new, dist in graph[curr].items():
+            if new not in seen:
+                mx = max(mx, dfs(new, set(seen)) + dist)
+        
+        return mx
+    
+    return dfs(start, set())
+
 
 with open(0) as file:
     data = file.read().rstrip().splitlines()
@@ -67,11 +106,12 @@ with open(0) as file:
 # for row in data:
 #     print(row)
 
+
 # Find all intersections
 # For each intersection find the distance to
 # the neighbouring intersections.
 # We now have a graph of distances between the intersections
-# Use dijkstras algorithm for longest path.
+# find the longest path in that graph.
 
 START = (0, data[0].index("."))
 STOP  = (len(data)-1, data[-1].index("."))
@@ -81,3 +121,31 @@ print(f"Part 1: {p1}")
 
 # p2 = None
 # print(f"Part 2: {p2}")
+
+intersections = {START, STOP}
+for r, row in enumerate(data):
+    for c, char in enumerate(row):
+        if char == ".":
+            total = 0
+            for d in DIRS:
+                new = add_points((r, c), d)
+                nr, nc = new
+                if not (0 <= nr < len(data) and 0 <= nc < len(data[0])):
+                    continue
+                if data[nr][nc] != '#':
+                    total += 1
+            if total >= 3:
+                intersections.add((r, c))
+
+print("================")
+GRAPH = dict()
+for cross in intersections:
+    res = walk_to_intersection(cross, intersections)
+    GRAPH[cross] = res
+
+for key, value in GRAPH.items():
+    print(key, value)
+
+print()
+
+print(longest_path(START, STOP, GRAPH))
