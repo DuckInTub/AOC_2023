9e3060d27174a6726c01280adde38bd88f087433
DuckInTub <icebl0804@gmail.com>
2023-12-25 23:01:16 +0100

    Messy hard-coded gradient descent, 24 part 2

--- 24/sol.py
+++ 24/sol.py
@@ -1,4 +1,6 @@
 from itertools import combinations, product
+from math import sqrt
+import numpy as np
 
 def sign(x):
     return round(x / abs(x))
@@ -38,5 +40,121 @@ for line1, line2 in combinations(data, 2):
         p1 += 1
 
 print(f"Part 1: {p1}")
+
+def parse(line):
+    x, y, z, dx, dy, dz = tuple(int(x) for x in line.replace("@", ",").split(","))
+    return (x, y, z), (dx, dy, dz)
+
+def norm(vec):
+    x, y, z = vec
+    L = sqrt(x ** 2 + y ** 2 + z ** 2)
+    return tuple(c / L for c in vec)
+
+def sub(v1, v2):
+    return tuple(c1 - c2 for c1, c2 in zip(v1, v2))
+
+# The rock we throws starting position P and its velocity V
+# Needs to be able to form euqations P+N*V = H + N*HV
+# For all hail stones H with velocity HV
+# such that N is an positive integer.
+pairs = []
+for line1, line2 in combinations(data, 2):
+    (_, _, _), (x, y, z) = parse(line1)
+    (_, _, _), (dx, dy, dz) = parse(line2)
+    n1 = norm((x, y, z))
+    n2 = norm((dx, dy, dz))
+    if all(abs(c) < 0.0001 for c in sub(n1, n2)):
+        pairs.append((line1, line2))
+        print(n1)
+        print(n2)
+        print(f"Line 1: {line1}, Line 2: {line2}")
+        print()
+    
+print(len(pairs))
+print("--------Part 2---------")
+
+use = data[:3]
+best = (0, 0, 0)
+mn = 10 ** 50
+best_sol = [10 ** 8]*2*len(use)
+for vx in range(312-25, 312+25, 1):
+    for vy in range(-116-25, -116+25, 1):
+        for vz in range(109-25, 109+25, 1):
+            # vx, vy, vz = -3, 1, 2
+            rows = []
+            consts = []
+            L = 3*len(use)
+            for i in range(len(use)):
+                (x, y, z), (dx, dy, dz) = parse(use[i])
+
+                row = [0]*(3+len(use))
+                row[0] = 1
+                row[3+i] = vx-dx
+                consts.append(x)
+                rows.append(row)
+
+                row = [0]*(3+len(use))
+                row[1] = 1
+                row[3+i] = vy-dy
+                consts.append(y)
+                rows.append(row)
+
+                row = [0]*(3+len(use))
+                row[2] = 1
+                row[3+i] = vz-dz
+                consts.append(z)
+                rows.append(row)
+            
+            mat = np.array(rows)
+            consts = np.array(consts)
+            try:
+                sol_pos, *rest = np.linalg.lstsq(mat, consts, rcond=1)
+                thing = rest[0][0]
+            except np.linalg.LinAlgError as e:
+                continue
+            if thing < mn:
+                best_sol = sol_pos
+                print(rest)
+                best = (vx, vy, vz)
+                print(best)
+                mn = thing
+                print(thing)
+
+print("------------------------")
+print("Min error: ", mn)
+print(best)
+print([float(x) for x in best_sol[:3]])
 # p2 = None
 # print(f"Part 2: {p2}")
+vx, vy, vz = (312, -116, 109)
+rows = []
+consts = []
+L = 3*len(use)
+for i in range(len(use)):
+    (x, y, z), (dx, dy, dz) = parse(use[i])
+
+    row = [0]*(3+len(use))
+    row[0] = 1
+    row[3+i] = vx-dx
+    consts.append(x)
+    rows.append(row)
+
+    row = [0]*(3+len(use))
+    row[1] = 1
+    row[3+i] = vy-dy
+    consts.append(y)
+    rows.append(row)
+
+    row = [0]*(3+len(use))
+    row[2] = 1
+    row[3+i] = vz-dz
+    consts.append(z)
+    rows.append(row)
+
+mat = np.array([row[:5] for row in rows][:5])
+print(mat)
+consts = np.array(consts[:5])
+sol = np.linalg.solve(mat, consts)
+sol = [float(x) for x in sol[:3]]
+assert all(int(x) == x for x in sol)
+print(sum([int(x) for x in sol[:3]]))
